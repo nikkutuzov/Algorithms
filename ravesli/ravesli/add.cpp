1899592b82350a3bb1517f4dc9cdc079a7d33d5b
nikkutuzov <nikkutuzov@outlook.com>
2021-07-31 17:50:15 +0400

    try add files

---
+++ ravesli/ravesli/add.cpp
@@ -0,0 +1,9 @@
+//
+//  add.cpp
+//  Ravesli
+//
+//  Created by Николай Кутузов on 28.06.2021.
+//
+
+#include <stdio.h>
+
---
+++ ravesli/ravesli/global.cpp
@@ -0,0 +1,6 @@
+//
+//  global.cpp
+//  Ravesli
+//
+//  Created by Николай Кутузов on 25.05.2021.
+//
